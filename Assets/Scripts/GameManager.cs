60ed2310bb6f863975166a0d2cbc695769890a99
B4ZE\wiaph <wi.aphikhun_st@tni.ac.th>
2023-03-09 21:39:57 +0700

    <Add> Mainmenu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,19 +9,19 @@ public class GameManager : MonoBehaviour
 {
     private int score;
     private float time;
-    [SerializeField] private Text hp_txt;
-    [SerializeField] private Text score_txt;
-    [SerializeField] private Text result_txt;
+    [SerializeField] private Text hp_txt = null;
+    [SerializeField] private Text score_txt = null;
+    [SerializeField] private Text result_txt = null;
 
     private bool isPause;
-    [SerializeField] private GameObject PausePanel;
+    [SerializeField] private GameObject PausePanel = null;
 
     private bool isShowInventory = true;
-    [SerializeField] private GameObject InventoryPanel;
+    [SerializeField] private GameObject InventoryPanel = null;
 
-    [SerializeField] private GameObject GameOverPanel;
+    [SerializeField] private GameObject GameOverPanel = null;
 
-    [SerializeField] private Text[] card_txt;
+    [SerializeField] private Text[] card_txt = null;
     // Start is called before the first frame update
     void Awake()
     {
@@ -108,6 +108,10 @@ public class GameManager : MonoBehaviour
         isPause = false;
         PausePanel.SetActive(false);
     }
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
     public void Exit()
     {
         Application.Quit();
@@ -115,6 +119,10 @@ public class GameManager : MonoBehaviour
     public void PlayAgain()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     private void ShowInventory()
     {
